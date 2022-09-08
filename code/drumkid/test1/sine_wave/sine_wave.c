3d24323caab70343ca2b1ad1d21d08c0637a9ac1
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2022-09-08 16:24:20 +0100

    seeing how much code i can remove and still get sound

--- code/drumkid/test1/sine_wave/sine_wave.c
+++ code/drumkid/test1/sine_wave/sine_wave.c
@@ -18,16 +18,12 @@
 
 #include "pico/audio_i2s.h"
 
-// MB adding Pimoroni Pico Audio pins
-#define PICO_AUDIO_I2S_DATA_PIN 9
-#define PICO_AUDIO_I2S_CLOCK_PIN_BASE 10
-
 #if PICO_ON_DEVICE
 #include "pico/binary_info.h"
 bi_decl(bi_3pins_with_names(PICO_AUDIO_I2S_DATA_PIN, "I2S DIN", PICO_AUDIO_I2S_CLOCK_PIN_BASE, "I2S BCK", PICO_AUDIO_I2S_CLOCK_PIN_BASE + 1, "I2S LRCK"));
 #endif
 
-#define SINE_WAVE_TABLE_LEN 8192
+#define SINE_WAVE_TABLE_LEN 16384
 #define SAMPLES_PER_BUFFER 256
 
 static int16_t sine_wave_table[SINE_WAVE_TABLE_LEN];
@@ -37,7 +33,7 @@ struct audio_buffer_pool *init_audio()
 
     static audio_format_t audio_format = {
         .format = AUDIO_BUFFER_FORMAT_PCM_S16,
-        .sample_freq = 24000,
+        .sample_freq = 48000,
         .channel_count = 1,
     };
 
@@ -50,8 +46,8 @@ struct audio_buffer_pool *init_audio()
     bool __unused ok;
     const struct audio_format *output_format;
     struct audio_i2s_config config = {
-        .data_pin = PICO_AUDIO_I2S_DATA_PIN,
-        .clock_pin_base = PICO_AUDIO_I2S_CLOCK_PIN_BASE,
+        .data_pin = 9,
+        .clock_pin_base = 10,
         .dma_channel = 0,
         .pio_sm = 0,
     };
@@ -75,36 +71,20 @@ int main()
 
     for (int i = 0; i < SINE_WAVE_TABLE_LEN; i++)
     {
-        sine_wave_table[i] = 0.5 * 32767 * (cosf(i * 2 * (float)(1 * M_PI / SINE_WAVE_TABLE_LEN)) + cosf(i * 2 * (float)(2 * M_PI / SINE_WAVE_TABLE_LEN)));
+        sine_wave_table[i] = 0.25 * 32767 * (cosf(i * 2 * (float)(4 * M_PI / SINE_WAVE_TABLE_LEN)) + cosf(i * 2 * (float)(5 * M_PI / SINE_WAVE_TABLE_LEN)));
     }
 
     struct audio_buffer_pool *ap = init_audio();
     uint32_t step = 0x200000;
     uint32_t pos = 0;
     uint32_t pos_max = 0x10000 * SINE_WAVE_TABLE_LEN;
-    uint vol = 128;
     while (true)
     {
-        int c = getchar_timeout_us(0);
-        if (c >= 0)
-        {
-            if (c == '-' && vol)
-                vol -= 4;
-            if ((c == '=' || c == '+') && vol < 255)
-                vol += 4;
-            if (c == '[' && step > 0x10000)
-                step -= 0x10000;
-            if (c == ']' && step < (SINE_WAVE_TABLE_LEN / 16) * 0x20000)
-                step += 0x10000;
-            if (c == 'q')
-                break;
-            printf("vol = %d, step = %d      \r", vol, step >> 16);
-        }
         struct audio_buffer *buffer = take_audio_buffer(ap, true);
         int16_t *samples = (int16_t *)buffer->buffer->bytes;
         for (uint i = 0; i < buffer->max_sample_count; i++)
         {
-            samples[i] = (vol * sine_wave_table[pos >> 16u]) >> 8u;
+            samples[i] = sine_wave_table[pos >> 16u];
             pos += step;
             if (pos >= pos_max)
                 pos -= pos_max;
@@ -112,6 +92,5 @@ int main()
         buffer->sample_count = buffer->max_sample_count;
         give_audio_buffer(ap, buffer);
     }
-    puts("\n");
     return 0;
 }
