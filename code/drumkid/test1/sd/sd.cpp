d6273726a6e73940eb0cbdb76fe5ac3110954c79
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2023-11-08 10:04:11 +0000

    sd card working in cpp

---
+++ code/drumkid/test1/sd/sd.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+//
+#include "f_util.h"
+#include "ff.h"
+#include "pico/stdlib.h"
+#include "rtc.h"
+//
+#include "hw_config.h"
+
+int main() {
+    stdio_init_all();
+    time_init();
+
+    sleep_ms(5000);
+
+    puts("Hello, world!");
+
+    // See FatFs - Generic FAT Filesystem Module, "Application Interface",
+    // http://elm-chan.org/fsw/ff/00index_e.html
+    sd_card_t *pSD = sd_get_by_num(0);
+    FRESULT fr = f_mount(&pSD->fatfs, pSD->pcName, 1);
+    if (FR_OK != fr) panic("f_mount error: %s (%d)\n", FRESULT_str(fr), fr);
+    FIL fil;
+    const char* const filename = "dknov8_1.txt";
+    fr = f_open(&fil, filename, FA_OPEN_APPEND | FA_WRITE);
+    if (FR_OK != fr && FR_EXIST != fr)
+        panic("f_open(%s) error: %s (%d)\n", filename, FRESULT_str(fr), fr);
+    if (f_printf(&fil, "Hello, world!\n") < 0) {
+        printf("f_printf failed\n");
+    }
+    fr = f_close(&fil);
+    if (FR_OK != fr) {
+        printf("f_close error: %s (%d)\n", FRESULT_str(fr), fr);
+    }
+    f_unmount(pSD->pcName);
+
+    puts("Goodbye, world!");
+
+    while(true) {
+        puts("test");
+        sleep_ms(500);
+    }
+
+    for (;;);
+}
