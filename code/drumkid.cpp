23d35c33433e2db4096dd1d26151e365b5b5a1e9
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2024-08-19 15:28:31 +0100

    bare minimum code working

--- code/drumkid.cpp
+++ code/drumkid.cpp
@@ -41,3 +41,300 @@ bi_decl(bi_3pins_with_names(PICO_AUDIO_I2S_DATA_PIN, "I2S DIN", PICO_AUDIO_I2S_C
 #include "Beat.h"
 #include "drumkid.h"
 
+// globals, organise later
+int64_t nextHitTime = 0; // in samples
+int64_t currentTime = 0; // in samples
+int scheduleAheadTime = SAMPLES_PER_BUFFER * 4; // in samples, was 0.02 seconds
+uint16_t scheduledStep = 0;
+int numSteps = 4 * QUARTER_NOTE_STEPS;
+
+void nextHit()
+{
+    scheduledStep++;
+
+    nextHitTime += stepTime;
+
+    if (scheduledStep >= numSteps)
+    {
+        scheduledStep = 0;
+    }
+}
+
+void scheduleHits()
+{
+    for (int i = 0; i < NUM_SAMPLES; i++)
+    {
+        if (beats[beatNum].getHit(i, scheduledStep, 0))
+        {
+            // if current beat contains a hit on this step, calculate the velocity and queue the hit
+            samples[i].queueHit(nextHitTime, scheduledStep, 4095);
+        }
+    }
+}
+
+void scheduler()
+{
+    while (beatPlaying && nextHitTime < currentTime + scheduleAheadTime)
+    {
+        scheduleHits();
+        nextHit();
+    }
+}
+
+bool tempScheduler(repeating_timer_t *rt)
+{
+    scheduler();
+    return true;
+}
+
+int main()
+{
+    stdio_init_all();
+    time_init();
+
+    printf("Drumkid V2\n");
+
+    findCurrentFlashSettingsSector();
+    initGpio();
+    initSamplesFromFlash();
+    loadBeatsFromFlash();
+
+    struct audio_buffer_pool *ap = init_audio();
+
+    int64_t tempPeriod = 500;
+    repeating_timer_t tempSchedulerTimer;
+    add_repeating_timer_us(tempPeriod, tempScheduler, NULL, &tempSchedulerTimer);
+
+    beatPlaying = true;
+
+    // audio buffer loop, runs forever
+    while (true)
+    {
+        struct audio_buffer *buffer = take_audio_buffer(ap, true);
+        int16_t *bufferSamples = (int16_t *)buffer->buffer->bytes;
+
+        // update audio output
+        for (uint i = 0; i < buffer->max_sample_count * 2; i += 2)
+        {
+            // sample updates go here
+            int32_t out1 = 0;
+            int32_t out2 = 0;
+
+            for (int j = 0; j < NUM_SAMPLES; j++)
+            {
+                bool didTrigger = samples[j].update(currentTime + (i >> 1)); // could be more efficient
+                if (true||samples[j].output1)
+                    out1 += samples[j].value;
+                if (samples[j].output2)
+                    out2 += samples[j].value;
+            }
+            bufferSamples[i] = out1 >> 2;
+            bufferSamples[i + 1] = rand() % 8192;
+        }
+        buffer->sample_count = buffer->max_sample_count;
+        give_audio_buffer(ap, buffer);
+        currentTime += SAMPLES_PER_BUFFER;
+    }
+    return 0;
+}
+
+#define FLASH_SETTINGS_START 0
+#define FLASH_SETTINGS_END 63
+int currentSettingsSector = 0;
+void findCurrentFlashSettingsSector()
+{
+    bool foundValidSector = false;
+    int mostRecentValidSector = 0;
+    int highestWriteNum = 0;
+    for (int i = FLASH_SETTINGS_START; i <= FLASH_SETTINGS_END; i++)
+    {
+        int testInt = getIntFromBuffer(flashData + i * FLASH_SECTOR_SIZE, DATA_CHECK);
+        if (testInt == CHECK_NUM)
+        {
+            foundValidSector = true;
+            int writeNum = getIntFromBuffer(flashData + i * FLASH_SECTOR_SIZE, 4);
+            if (writeNum >= highestWriteNum)
+            {
+                highestWriteNum = writeNum;
+                mostRecentValidSector = i;
+            }
+        }
+    }
+    if (!foundValidSector)
+    {
+        // no valid sectors, which means the flash needs to be initialised:
+        /*uint8_t buffer[FLASH_PAGE_SIZE];
+        int32_t refCheckNum = CHECK_NUM;
+        std::memcpy(buffer, &refCheckNum, 4); // copy check number
+        int32_t refZero = 0;
+        std::memcpy(buffer + 4, &refZero, 4); // copy number zero, the incremental write number for wear levelling
+        uint32_t dummySampleLength = 1024;
+        for (int i = 0; i < NUM_SAMPLES; i++)
+        {
+            uint32_t dummySampleStart = i * dummySampleLength * 2;
+            std::memcpy(buffer + SAMPLE_START_POINTS + 4 * i, &dummySampleStart, 4);
+            std::memcpy(buffer + SAMPLE_LENGTHS + 4 * i, &dummySampleLength, 4);
+        }
+        writePageToFlash(buffer, FLASH_DATA_ADDRESS + FLASH_SETTINGS_START * FLASH_SECTOR_SIZE);
+
+        // fill audio buffer with random noise
+        for (int i = 0; i < dummySampleLength * 2 * NUM_SAMPLES; i += FLASH_PAGE_SIZE)
+        {
+            for (int j = 0; j < FLASH_PAGE_SIZE; j++)
+            {
+                buffer[j] = rand() % 256; // random byte
+            }
+            writePageToFlash(buffer, FLASH_AUDIO_ADDRESS + i);
+        }
+
+        saveSettings();*/
+    }
+    else
+    {
+        currentSettingsSector = mostRecentValidSector;
+    }
+}
+
+void initSamplesFromFlash()
+{
+    int startPos = 0;
+    int storageOverflow = false;
+    for (int n = 0; n < NUM_SAMPLES; n++)
+    {
+        uint sampleStart = getIntFromBuffer(flashAudioMetadata, SAMPLE_START_POINTS + n * 4);
+        uint sampleLength = getIntFromBuffer(flashAudioMetadata, SAMPLE_LENGTHS + n * 4);
+        printf("start %d, length %d\n", sampleStart, sampleLength);
+
+        samples[n].length = sampleLength / 2; // divide by 2 to go from 8-bit to 16-bit
+        samples[n].startPosition = sampleStart / 2;
+        if (samples[n].startPosition + samples[n].length >= MAX_SAMPLE_STORAGE)
+        {
+            samples[n].startPosition = 0;
+            samples[n].length = 1000;
+            storageOverflow = true;
+        }
+        printf("sample %d, start %d, length %d\n", n, samples[n].startPosition, samples[n].length);
+        samples[n].position = samples[n].length;
+        samples[n].positionAccurate = samples[n].length << Sample::LERP_BITS;
+
+        if (!storageOverflow)
+        {
+            for (int i = 0; i < samples[n].length && i < sampleLength / 2; i++)
+            {
+                int pos = i * 2 + sampleStart;
+                int16_t thisSample = flashAudio[pos + 1] << 8 | flashAudio[pos];
+                Sample::sampleData[sampleStart / 2 + i] = thisSample;
+            }
+        }
+    }
+
+    if (storageOverflow)
+    {
+        //showError("size");
+    }
+}
+
+void loadBeatsFromFlash()
+{
+    int startPos = 0;
+    for (int i = 0; i < NUM_BEATS; i++)
+    {
+        for (int j = 0; j < NUM_SAMPLES; j++)
+        {
+            std::memcpy(&beats[i].beatData[j], &flashUserBeats[8 * (i * NUM_SAMPLES + j)], 8);
+        }
+    }
+    backupBeat(beatNum);
+}
+
+void backupBeat(int backupBeatNum)
+{
+    for (int i = 0; i < NUM_SAMPLES; i++)
+    {
+        beatBackup.beatData[i] = beats[backupBeatNum].beatData[i];
+    }
+}
+
+void initGpio()
+{
+    adc_init();
+    adc_gpio_init(MUX_READ_POTS);
+    adc_gpio_init(MUX_READ_CV);
+    gpio_init(MUX_ADDR_A);
+    gpio_set_dir(MUX_ADDR_A, GPIO_OUT);
+    gpio_init(MUX_ADDR_B);
+    gpio_set_dir(MUX_ADDR_B, GPIO_OUT);
+    gpio_init(MUX_ADDR_C);
+    gpio_set_dir(MUX_ADDR_C, GPIO_OUT);
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_init(DATA_595);
+    gpio_set_dir(DATA_595, GPIO_OUT);
+    gpio_init(CLOCK_595);
+    gpio_set_dir(CLOCK_595, GPIO_OUT);
+    gpio_init(LATCH_595);
+    gpio_set_dir(LATCH_595, GPIO_OUT);
+    gpio_init(LOAD_165);
+    gpio_set_dir(LOAD_165, GPIO_OUT);
+    gpio_init(CLOCK_165);
+    gpio_set_dir(CLOCK_165, GPIO_OUT);
+    gpio_init(DATA_165);
+    gpio_set_dir(DATA_165, GPIO_IN);
+    gpio_init(SYNC_OUT);
+    gpio_set_dir(SYNC_OUT, GPIO_OUT);
+    gpio_init(SYNC_IN);
+    gpio_set_dir(SYNC_IN, GPIO_IN);
+    for (int i = 0; i < 4; i++)
+    {
+        gpio_init(TRIGGER_OUT_PINS[i]);
+        gpio_set_dir(TRIGGER_OUT_PINS[i], GPIO_OUT);
+    };
+
+    // set GPIO23 high, apparently improves ADC readings
+    gpio_init(23);
+    gpio_set_dir(23, GPIO_OUT);
+    gpio_put(23, 1);
+}
+
+// Borrowed/adapted from pico-playground
+struct audio_buffer_pool *init_audio()
+{
+
+    static audio_format_t audio_format = {
+        SAMPLE_RATE,
+        AUDIO_BUFFER_FORMAT_PCM_S16,
+        2,
+    };
+
+    static struct audio_buffer_format producer_format = {
+        .format = &audio_format,
+        .sample_stride = 4};
+
+    struct audio_buffer_pool *producer_pool = audio_new_producer_pool(&producer_format, 3, SAMPLES_PER_BUFFER); // todo correct size
+    bool __unused ok;
+    const struct audio_format *output_format;
+    struct audio_i2s_config config = {
+        .data_pin = 9,
+        .clock_pin_base = 10,
+        .dma_channel = 2, // was 0, trying to avoid SD conflict
+        .pio_sm = 0,
+    };
+
+    output_format = audio_i2s_setup(&audio_format, &config);
+    if (!output_format)
+    {
+        panic("PicoAudio: Unable to open audio device.\n");
+    }
+
+    ok = audio_i2s_connect(producer_pool);
+    assert(ok);
+    audio_i2s_set_enabled(true);
+    return producer_pool;
+}
+
+int32_t getIntFromBuffer(const uint8_t *buffer, uint position)
+{
+    // potentially a silly way of doing this, written before I knew about std::memcpy? but it works
+    int32_t thisInt = buffer[position] | buffer[position + 1] << 8 | buffer[position + 2] << 16 | buffer[position + 3] << 24;
+    return thisInt;
+}
---
+++ code/drumkid.h
@@ -0,0 +1,398 @@
+#ifndef DK_H
+#define DK_H
+
+#include "constants.h"
+
+// Borrowing some useful Arduino macros, but updating from 1UL to 1ULL for 64-bit compatibility
+#define bitRead(value, bit) (((value) >> (bit)) & 0x01)
+#define bitSet(value, bit) ((value) |= (1ULL << (bit)))
+#define bitClear(value, bit) ((value) &= ~(1ULL << (bit)))
+#define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
+
+#define SAMPLES_PER_BUFFER 32 // 32 works well
+
+// pins (updated for PCB 2.02)
+#define MUX_ADDR_A 19
+#define MUX_ADDR_B 20
+#define MUX_ADDR_C 21
+#define MUX_READ_POTS 26
+#define MUX_READ_CV 27
+#define LED_PIN 25
+#define DATA_595 6
+#define CLOCK_595 7
+#define LATCH_595 8
+#define LOAD_165 12
+#define CLOCK_165 13
+#define DATA_165 14
+#define SYNC_IN 16
+#define SYNC_OUT 17
+#define BUTTON_PIN_START_STOP 0
+#define BUTTON_PIN_TAP_TEMPO 1
+const uint8_t TRIGGER_OUT_PINS[4] = {15, 28, 22, 18};
+
+// button numbers (shifted buttons are +16)
+#define BUTTON_CANCEL 5
+#define BUTTON_CONFIRM 6
+#define BUTTON_DEC 7
+#define BUTTON_INC 8
+#define BUTTON_SHIFT_CANCEL 9
+#define BUTTON_CLEAR 10
+#define BUTTON_TIME_SIGNATURE 11
+#define BUTTON_LOAD_SAMPLES 12
+#define BUTTON_SETTINGS 13
+#define BUTTON_B 14
+#define BUTTON_TUPLET 15
+#define BUTTON_MANUAL_TEMPO 16
+#define BUTTON_BEAT 17
+#define BUTTON_SAVE 18
+#define BUTTON_A 19
+#define BUTTON_EDIT_BEAT 20
+#define BUTTON_LIVE_EDIT 21
+#define BUTTON_TAP_TEMPO 22
+#define BUTTON_START_STOP 23
+#define NO_ACTIVE_BUTTON -1
+#define ERROR_DISPLAY -2
+#define BOOTUP_VISUALS -3
+
+// menu settings
+#define NUM_MENU_SETTINGS 9
+#define SETTING_CLOCK_MODE 0
+#define SETTING_OUTPUT_1 1
+#define SETTING_OUTPUT_2 2
+#define SETTING_GLITCH_CHANNEL 3
+#define SETTING_OUTPUT_PULSE_LENGTH 4
+#define SETTING_OUTPUT_PPQN 5
+#define SETTING_INPUT_PPQN 6
+#define SETTING_PITCH_CURVE 7
+#define SETTING_INPUT_QUANTIZE 8
+
+// non-menu settings
+#define SETTING_BEAT 8
+#define SETTING_TEMPO 9
+#define SETTING_TUPLET 10
+#define SETTING_TIME_SIG 11
+
+#define GLITCH_CHANNEL_BOTH 0
+#define GLITCH_CHANNEL_1 1
+#define GLITCH_CHANNEL_2 2
+#define GLITCH_CHANNEL_NONE 3
+
+#define PITCH_CURVE_DEFAULT 0
+#define PITCH_CURVE_LINEAR 1
+#define PITCH_CURVE_FORWARDS 2
+
+// pot numbers
+#define POT_CHANCE 0
+#define POT_ZOOM 1
+#define POT_CLUSTER 2
+#define POT_CROP 3
+#define POT_MIDPOINT 4
+#define POT_RANGE 5
+#define POT_MAGNET 6
+#define POT_SWING 7
+#define POT_CRUSH 8
+#define POT_PITCH 9
+#define POT_DROP_RANDOM 10
+#define POT_DROP 11
+
+// CV input numbers
+#define CV_CHANCE 12
+#define CV_ZOOM 15
+#define CV_MIDPOINT 13
+#define CV_TBC 14
+
+// tuplet modes
+#define TUPLET_STRAIGHT 0
+#define TUPLET_TRIPLET 1
+#define TUPLET_QUINTUPLET 2
+#define TUPLET_SEPTUPLET 3
+
+// analog smoothing etc
+#define ANALOG_DEAD_ZONE_LOWER 100
+#define ANALOG_DEAD_ZONE_UPPER (4095 - ANALOG_DEAD_ZONE_LOWER)
+#define ANALOG_EFFECTIVE_RANGE (ANALOG_DEAD_ZONE_UPPER - ANALOG_DEAD_ZONE_LOWER)
+
+// flash data storage
+#define FLASH_DATA_ADDRESS (1024 * 1024)
+#define FLASH_USER_BEATS_ADDRESS (FLASH_DATA_ADDRESS + 64 * FLASH_SECTOR_SIZE)
+#define FLASH_AUDIO_METADATA_ADDRESS (FLASH_DATA_ADDRESS + 127 * FLASH_SECTOR_SIZE)
+#define FLASH_AUDIO_ADDRESS (FLASH_DATA_ADDRESS + 128 * FLASH_SECTOR_SIZE)
+const uint8_t *flashData = (const uint8_t *)(XIP_BASE + FLASH_DATA_ADDRESS);
+const uint8_t *flashUserBeats = (const uint8_t *)(XIP_BASE + FLASH_USER_BEATS_ADDRESS);
+const uint8_t *flashAudio = (const uint8_t *)(XIP_BASE + FLASH_AUDIO_ADDRESS);
+const uint8_t *flashAudioMetadata = (const uint8_t *)(XIP_BASE + FLASH_AUDIO_METADATA_ADDRESS);
+#define CHECK_NUM -999999
+#define DATA_CHECK 0
+#define SAMPLE_START_POINTS 4
+#define SAMPLE_LENGTHS (SAMPLE_START_POINTS + 8 * 4)
+
+// Beat variables
+#define NUM_BEATS 16
+#define MASTER_PPQN 3360                                    // lowest common multiple of 32, 24, 15, and 28 (the highest PPQN values used for the various tuplet modes) - currently unused, really just an aspiration to use a less stupid timing system than the current horrible one
+uint16_t tempo = 150;                                       // BPM
+uint64_t stepTime = 2646000 / (tempo * QUARTER_NOTE_STEPS); // microseconds
+int samplesPerStep;                                         // slower tempos give higher values
+uint32_t SAMPLE_RATE = 44100;
+bool beatPlaying = false;
+int beatNum = 6;
+int saveBeatLocation;
+Beat beats[NUM_BEATS];
+Beat beatBackup; // copy of current beat before editing, to allow easy revert if not saving new version
+Sample samples[NUM_SAMPLES];
+int editSample = 0;
+int editStep = 0;
+
+// SD card stuff
+#define MAX_SAMPLE_FOLDERS 16
+#define MAX_FOLDER_NAME_LENGTH 16
+char folderNames[MAX_SAMPLE_FOLDERS][MAX_FOLDER_NAME_LENGTH];
+char path[] = "samples/";
+int numSampleFolders = 0;
+
+// temporary (ish?) LED variables (first 8 bits are the segments, next 4 are the character selects, final 4 are 3mm LEDs)
+#define LED_PULSE_LENGTH 10000
+uint8_t sevenSegData[4] = {0b00000000, 0b00000000, 0b00000000, 0b00000000};
+uint8_t singleLedData = 0b0000; // 4 x 3mm LEDs
+uint16_t storedLedData[4] = {0, 0, 0, 0};
+uint8_t sevenSegCharacters[10] = {
+    0b11111100,
+    0b01100000,
+    0b11011010,
+    0b11110010,
+    0b01100110,
+    0b10110110,
+    0b10111110,
+    0b11100000,
+    0b11111110,
+    0b11110110};
+uint8_t sevenSegAlphaCharacters[26] = {
+    0b11101110,
+    0b00111110,
+    0b10011100,
+    0b01111010,
+    0b10011110,
+    0b10001110,
+    0b11110110,
+    0b01101110,
+    0b01100000,
+    0b01111000,
+    0b01101110,
+    0b00011100,
+    0b11101100,
+    0b00101010,
+    0b00111010,
+    0b11001110,
+    0b11100110,
+    0b00001010,
+    0b10110110,
+    0b00011110,
+    0b01111100,
+    0b00111000,
+    0b01111100,
+    0b01101110,
+    0b01110110,
+    0b11011010};
+uint8_t sevenSegAsciiCharacters[256] = {
+    0b00000000, // 0 NULL
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000, // 32 SPACE
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000010, // -
+    0b00000000,
+    0b00000000,
+    0b11111100, // digit 0
+    0b01100000,
+    0b11011010,
+    0b11110010,
+    0b01100110,
+    0b10110110,
+    0b10111110,
+    0b11100000,
+    0b11111110,
+    0b11110110, // digit 9
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000, // 64
+    0b11101110, // A (a)
+    0b00111110,
+    0b10011100,
+    0b01111010,
+    0b10011110,
+    0b10001110,
+    0b11110110,
+    0b01101110,
+    0b01100000,
+    0b01111000,
+    0b01101110,
+    0b00011100,
+    0b11101100,
+    0b00101010,
+    0b00111010,
+    0b11001110,
+    0b11100110,
+    0b00001010,
+    0b10110110,
+    0b00011110,
+    0b01111100,
+    0b00111000,
+    0b01111100,
+    0b01101110,
+    0b01110110,
+    0b11011010, // Z (z)
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00010000, // _
+    0b00000000, // 96
+    0b11101110, // a
+    0b00111110,
+    0b10011100,
+    0b01111010,
+    0b10011110,
+    0b10001110,
+    0b11110110,
+    0b01101110,
+    0b01100000,
+    0b01111000,
+    0b01101110,
+    0b00011100,
+    0b11101100,
+    0b00101010,
+    0b00111010,
+    0b11001110,
+    0b11100110,
+    0b00001010,
+    0b10110110,
+    0b00011110,
+    0b01111100,
+    0b00111000,
+    0b01111100,
+    0b01101110,
+    0b01110110,
+    0b11011010, // z
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+    0b00000000,
+};
+
+// timers and alarms
+repeating_timer_t mainTimer;
+repeating_timer_t performanceCheckTimer;
+
+uint analogLoopNum = 0; // 0 to 7
+uint analogPhase = 0;   // 0 or 1
+uint analogReadings[16] = {0};
+
+uint shiftRegOutLoopNum = 0; // 0 to 15
+uint shiftRegOutPhase = 0;   // 0, 1, or 2
+uint sevenSegCharNum = 0;
+
+bool buttonStableStates[24] = {false}; // array of bools not good use of space, change if running out of space
+uint32_t microsSinceChange[24] = {0};  // milliseconds since state change
+uint shiftRegInLoopNum = 0;            // 0 to 15
+uint shiftRegInPhase = 0;              // 0 or 1
+
+int outputPulseLength = 15;
+int pitchCurve = PITCH_CURVE_DEFAULT;
+const int NUM_QUANTIZE_VALUES = 4;
+int quantizeValues[NUM_QUANTIZE_VALUES] = {4, 8, 16, 32};
+int inputQuantizeIndex = 0;
+uint inputQuantize = quantizeValues[inputQuantizeIndex];
+
+void initGpio();
+bool performanceCheck(repeating_timer_t *rt);
+bool mainTimerLogic(repeating_timer_t *rt);
+struct audio_buffer_pool *init_audio();
+char getNthDigit(int x, int n);
+void updateLedDisplay(int num);
+void updateLedDisplayAlpha(const char *word);
+void handleIncDec(bool isInc, bool isHold);
+void handleYesNo(bool isYes);
+void displayClockMode();
+void displayTempo();
+void displayBeat();
+void displayEditBeat();
+void displayTimeSignature();
+void displayTuplet();
+void displayOutput(int outputNum);
+void displaySettings();
+void handleButtonChange(int buttonNum, bool buttonState);
+void updateShiftRegButtons();
+void updateAnalog();
+void getNthSampleFolder(int n);
+void scanSampleFolders();
+void loadSamplesFromSD();
+void updateLeds();
+void pulseGpio(uint gpioNum, uint16_t pulseLengthMicros);
+void pulseLed(uint ledNum, uint16_t pulseLengthMicros);
+void initSamplesFromFlash();
+void loadBeatsFromFlash();
+void writeBeatsToFlash();
+void writePageToFlash(const uint8_t *buffer, uint address);
+void checkFlashData();
+int32_t getIntFromBuffer(const uint8_t *buffer, uint position);
+float getFloatFromBuffer(const uint8_t *buffer, uint position);
+void updateSyncIn();
+void updateTapTempo();
+void doStep();
+void print_buf(const uint8_t *buf, size_t len);
+void initZoom();
+void displayPulse();
+void revertBeat(int revertBeatNum);
+void backupBeat(int backupBeatNum);
+void showError(const char *msgx);
+void findCurrentFlashSettingsSector();
+void saveSettings();
+void loadSettings();
+bool checkSettingsChange();
+void scheduleSaveSettings();
+
+#endif
